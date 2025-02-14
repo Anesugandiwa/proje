9d23b4d99db45f889e5b23f21a5c71ae4d13543e
Anesu Gandiwa <148047827+Anesugandiwa@users.noreply.github.com>
2025-02-14 17:11:52 +0200

    user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-            'password' =>Hash::make('Anesu@gmail.com')
+            'password' =>Hash::make('Anesu@gmail.com'),
+            'username' =>'testuser',
         ]);
     }
 }
